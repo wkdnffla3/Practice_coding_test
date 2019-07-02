1ab411b374655df7226ba290807d8627493f74a7
wkdnffla3 <wkdnffla3@>
2019-07-02 19:37:04 +0900

    update my directory

--- test.cpp
+++ test.cpp
@@ -1,1 +1,22 @@
 #include<stdio.h>
+#include<vector>
+#include<string>
+#include<iostream>
+using namespace std;
+//using namespace string;
+int main() {
+	string str = "asdasdasd";
+	string temp = "";
+	for (int i = 0; str[i]; i++) {
+		//cout << str[i]-'a' << endl;
+		//temp[i] += 'a';
+	}
+	//	temp[9] = '\0';
+	temp += "aasdasdasd";
+
+	//temp[1] = 'a';
+	cout << temp;
+
+
+	return 0;
+}
