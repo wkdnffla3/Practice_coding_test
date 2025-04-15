e7c3edac2d038cd81b1fb238a2e4d76384b91b0f
CG_Pack <wkdnffla3@korea.ac.kr>
2025-04-15 19:43:29 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

---
+++ Java/백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        System.out.println(A+B);
+    }
+}
