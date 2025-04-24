80ba82ba6fdb5e2016af2198732f8676e47b4ab4
CG_Pack <wkdnffla3@korea.ac.kr>
2025-04-24 20:04:18 +0900

    [Bronze III] Title: 별 찍기 - 12, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
+++ C++17/백준/Bronze/2522. 별 찍기 － 12/별 찍기 － 12.cc
@@ -1,21 +1,18 @@
 #include<iostream>
-
+#include<math.h>
 using namespace std;
 
 int main(){
 int N;
 cin>> N;
 
-for (int i=0; i<N;i++){
-for(int j = N-1-i; j>0; j--) cout<< " ";
-for ( int j =N-1 -i ; j < N ; j++){cout<<"*";}
-cout<<"\n";
-}
-for(int i=0 ; i<N-1;i++){
-for ( int j =N-1 -i ; j < N ; j++){cout<<" ";}
-for(int j = N-1-i; j>0; j--) cout<< "*";
+for(int i=0 ; i<N*2 -1 ;i++){
+    for(int j = 0 ; j < abs(N-i-1);j++)
+        cout << " ";
+    for(int j=0 ; j<N -abs(N-i-1);j++ )
+        cout<<"*";
     cout<<"\n";
-
+    
 }
 
 
